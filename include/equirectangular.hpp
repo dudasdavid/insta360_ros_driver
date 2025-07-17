a3486ef0b49bc7ee9b2176353c223658ce783934
David Dudas <david.dudas@outlook.com>
2025-07-17 18:13:05 +0200

    switch back to local uncompressed image processing

--- include/equirectangular.hpp
+++ include/equirectangular.hpp
@@ -19,7 +19,7 @@ public:
 
 private:
     // Callback functions
-    void imageCallback(const sensor_msgs::msg::CompressedImage::SharedPtr msg);
+    void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
     rcl_interfaces::msg::SetParametersResult parametersCallback(const std::vector<rclcpp::Parameter> &parameters);
     
     // Initialization functions
@@ -31,7 +31,7 @@ private:
     cv::Mat createEquirectangular(const cv::Mat& front_img, const cv::Mat& back_img);
     
     // ROS2 communication
-    rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr dual_fisheye_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr dual_fisheye_sub_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr equirect_pub_;
     rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr equirect_compressed_pub_;
 
--- scripts/equirectangular.py
+++ scripts/equirectangular.py
@@ -87,9 +87,9 @@ class EquirectangularNode(Node):
         )
         
         self.dual_fisheye_sub = self.create_subscription(
-            Image, '/dual_fisheye/image', self.image_callback, qos)
+            Image, '/insta360_air/dual_fisheye/image', self.image_callback, qos)
         self.equirect_pub = self.create_publisher(
-            Image, '/equirectangular/image', qos)
+            Image, '/insta360_air/equirectangular/image', qos)
         
         if self.calibration_mode:
             self.get_logger().info("Calibration mode enabled")
@@ -347,8 +347,8 @@ class EquirectangularNode(Node):
             front_img_full = dual_fisheye_img[:, midpoint:]
             back_img_full = dual_fisheye_img[:, :midpoint]
 
-            front_img_full = cv2.rotate(front_img_full, cv2.ROTATE_90_COUNTERCLOCKWISE)
-            back_img_full = cv2.rotate(back_img_full, cv2.ROTATE_90_CLOCKWISE)
+            #front_img_full = cv2.rotate(front_img_full, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            #back_img_full = cv2.rotate(back_img_full, cv2.ROTATE_90_CLOCKWISE)
             
             # Store original uncropped images (always update in calibration mode)
             if self.calibration_mode or self.original_front_img is None or self.original_front_img.shape != front_img_full.shape:
